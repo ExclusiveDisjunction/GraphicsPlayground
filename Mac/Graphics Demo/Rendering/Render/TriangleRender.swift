32dde83fad0c8551510eb490c51e8ba43228482e
exdisj <exdisj@outlook.com>
2025-07-08 20:46:44 -0400

    Worked on the new UI

--- Mac/Graphics Demo/Rendering/Render/TriangleRender.swift
+++ Mac/Graphics Demo/Rendering/Render/TriangleRender.swift
@@ -10,21 +10,12 @@ import MetalKit
 import SwiftUI
 import ExdisjGraphics
 
-final class TriangleRender : NSObject, MTKViewDelegate {
-    var device: MTLDevice;
-    var commandQueue: MTLCommandQueue;
+final class TriangleRender : RendererBasis, MTKViewDelegate {
     var pipeline: MTLRenderPipelineState;
     
-    init(_ device: MTLDevice) throws(MissingMetalComponentError) {
-        self.device = device;
-        
-        guard let commandQueue = device.makeCommandQueue() else {
-            throw .commandQueue
-        }
-        self.commandQueue = commandQueue
-        
+    override init(_ device: MTLDevice) throws(MissingMetalComponentError) {
         do {
-            self.pipeline = try Self.buildPipeline(device: self.device)
+            self.pipeline = try Self.buildPipeline(device: device)
         }
         catch let e as MissingMetalComponentError {
             throw e
@@ -33,7 +24,7 @@ final class TriangleRender : NSObject, MTKViewDelegate {
             throw .pipeline(e)
         }
         
-        super.init()
+        try super.init(device)
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -41,47 +32,24 @@ final class TriangleRender : NSObject, MTKViewDelegate {
     }
     
     func draw(in view: MTKView) {
-        guard let drawable = view.currentDrawable,
-              let commandBuffer = commandQueue.makeCommandBuffer(),
-              let renderPassDescriptor = view.currentRenderPassDescriptor else {
-            print("key components for rendering could not be made")
+        guard let context = FrameDrawContext(view: view, queue: commandQueue) else {
             return;
         }
         
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0.5, 0.5, 1.0);
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear;
-        renderPassDescriptor.colorAttachments[0].storeAction = .store;
+        context.setColorAttachments()
         
-        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
-            print("unable to create the render encoder")
+        guard let renderEncoder = context.makeEncoder() else {
             return;
         }
         
         renderEncoder.setRenderPipelineState(pipeline)
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3);
-        renderEncoder.endEncoding()
         
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
+        renderEncoder.endEncoding()
+        context.commit()
     }
     
     static func buildPipeline(device: MTLDevice) throws -> MTLRenderPipelineState {
-        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-        guard let library = device.makeDefaultLibrary() else {
-            throw MissingMetalComponentError.defaultLibrary
-        }
-        
-        guard let vertexFunction = library.makeFunction(name: "triangleVertexMain") else {
-            throw MissingMetalComponentError.libraryFunction("triangleVertexMain")
-        }
-        guard let fragmentFunction = library.makeFunction(name: "triangleFragmentMain") else {
-            throw MissingMetalComponentError.libraryFunction("triangleFragmentMain")
-        }
-        
-        pipelineDescriptor.vertexFunction = vertexFunction
-        pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm;
-        
-        return try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        return try self.makeSimplePipeline(device, vertex: "triangleVertexMain", fragment: "triangleFragmentMain", is2d: true)
     }
 }
